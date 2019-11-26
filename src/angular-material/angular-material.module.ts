a84243da19f231317bd9c8a0ad292f85002d80e1
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-26 14:18:05 +0100

    3 files modified, 1 added

--- src/angular-material/angular-material.module.ts
+++ src/angular-material/angular-material.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { MatCardModule } from "@angular/material";
+import { MatCardModule, MatButtonModule } from "@angular/material";
 
 @NgModule({
-  imports: [CommonModule, MatCardModule],
-  declarations: []
+  imports: [MatCardModule, MatButtonModule],
+  exports: [MatCardModule, MatButtonModule]
 })
 export class AngularMaterialModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { TeamModule } from '../team/team.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, TeamModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, TeamModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
