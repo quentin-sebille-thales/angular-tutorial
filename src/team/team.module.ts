8639fcb7410511a8ca667f17490b93bf7606c8e9
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-26 14:22:42 +0100

    2 files modified, 1 added

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -4,10 +4,12 @@ import { AngularMaterialModule } from "../angular-material/angular-material.modu
 
 import { TeamCardComponent } from "./components/team-card/team-card.component";
 import { TeamPageComponent } from "./components/team-page/team-page.component";
+import { TeamService } from './services/team.service';
 
 @NgModule({
   imports: [CommonModule, AngularMaterialModule],
   declarations: [TeamCardComponent, TeamPageComponent],
-  exports: [TeamPageComponent]
+  exports: [TeamPageComponent],
+  providers: [TeamService]
 })
 export class TeamModule {}
