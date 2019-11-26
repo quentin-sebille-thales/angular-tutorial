3830382a9766430264bd3484507705386e94eec5
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-26 16:13:29 +0100

    2 files modified

--- src/team/components/team-card/team-card.component.ts
+++ src/team/components/team-card/team-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { Team } from "../../models/team";
 
 @Component({
@@ -15,25 +15,27 @@ export class TeamCardComponent implements OnInit {
    * Le composant doit-il être affiché ?
    */
   displayComponent: boolean;
+  /**
+   * Evénement emis lors de l'évaluation de l'équipe.
+   */
+  @Output() evaluate: EventEmitter<Team>;
 
   constructor() {
     console.log("Equipe (Constructor) : " + this.team);
     this.displayComponent = true;
+    this.evaluate = new EventEmitter();
   }
 
   ngOnInit(): void {
     console.log("Equipe (OnInit) : " + this.team);
-    this.displayComponent =
-      this.team !== null &&
-      this.team !== undefined;
+    this.displayComponent = this.team !== null && this.team !== undefined;
   }
 
-/**
- * Action déclenchée lors du clic sur le bouton "Evaluer"
- */
+  /**
+   * Action déclenchée lors du clic sur le bouton "Evaluer"
+   */
   onClickEvaluate(): void {
-
+    this.evaluate.emit(this.team);
   }
-
 
 }
