22f3aaba9f699b3c0bfa062bb4c90433a8587bcf
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-27 10:21:32 +0100

    2 files added

---
+++ src/routing/routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from "@angular/router";
+import { TeamPageComponent } from "../team/components/team-page/team-page.component";
+
+export const routes: Routes = [{ path: "team", component: TeamPageComponent }];
