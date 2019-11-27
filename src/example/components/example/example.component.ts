1de23ee627cba107b7e13a7ead565090471a6b2f
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-27 20:17:38 +0100

    1 file modified

--- src/example/components/example/example.component.ts
+++ src/example/components/example/example.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ExampleService } from "../../services/example.service";
 
+/**
+ * Exemple de composant Angular
+ */
 @Component({
-  selector: 'app-example',
-  templateUrl: './example.component.html',
-  styleUrls: ['./example.component.css']
+  /**
+   * Le selector : ce qui permet d'appeler le composant dans la vue.
+   */
+  selector: "app-example",
+  /**
+   * Le fichier (ou le contenu) HTML associé à ce composant
+   */
+  templateUrl: "./example.component.html",
+  /**
+   * Le style associé à ce composant
+   */
+  styleUrls: ["./example.component.css"]
 })
 export class ExampleComponent implements OnInit {
+  /**
+   * Constructeur du composant (de la classe).
+   * Les services sont déclarés en argument.
+   */
+  constructor(private exampleService: ExampleService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  /**
+   * Fonction appelée à l'initialisation du composant
+   */
+  ngOnInit(): void {}
 }
