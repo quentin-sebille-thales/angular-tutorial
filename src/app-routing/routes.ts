dd69f9cf5a0576c4703afaca8d8194f4f948ed2e
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-27 10:37:00 +0100

    4 files modified, 4 deleted

--- src/app-routing/routes.ts
+++ src/app-routing/routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from "@angular/router";
 import { TeamPageComponent } from "../team/components/team-page/team-page.component";
+import { TaskPageComponent } from "../task/components/task-page/task-page.component";
 
-export const routes: Routes = [{ path: "team", component: TeamPageComponent }];
+
+export const routes: Routes = [{ path: "team", component: TeamPageComponent }, {path: "task", component: TaskPageComponent}];
