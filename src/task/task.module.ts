21e356a5f9e3b11bd0410372f363696b08123f5a
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-27 10:38:18 +0100

    1 file modified, 3 added

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { TaskPageComponent } from "./components/task-page/task-page.component";
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule],
+  declarations: [TaskPageComponent],
+  exports: [TaskPageComponent]
 })
-export class TaskModule { }
+export class TaskModule {}
