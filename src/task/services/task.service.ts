d9631e9cfe3981436397c2db3818e676d2d92b90
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-27 11:02:37 +0100

    2 files modified, 2 added

---
+++ src/task/services/task.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+import { Task } from "../models/task";
+
+@Injectable()
+export class TaskService {
+  list: Task[];
+
+  constructor() {}
+}
--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from "@angular/common";
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
 
 import { TaskPageComponent } from "./components/task-page/task-page.component";
+import { TaskService } from './services/task.service';
 
 @NgModule({
   imports: [CommonModule, AngularMaterialModule],
   declarations: [TaskPageComponent],
-  exports: [TaskPageComponent]
+  exports: [TaskPageComponent],
+  providers: [TaskService]
 })
 export class TaskModule {}
