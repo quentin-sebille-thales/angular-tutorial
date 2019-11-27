67dfa45aae7eb7c8b130c962635ee72be05a1c9e
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-27 11:50:42 +0100

    3 files modified, 3 added

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -5,10 +5,11 @@ import { AngularMaterialModule } from "../angular-material/angular-material.modu
 import { TaskPageComponent } from "./components/task-page/task-page.component";
 import { TaskService } from './services/task.service';
 import { TaskCardComponent } from './components/task-card/task-card.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
 
 @NgModule({
   imports: [CommonModule, AngularMaterialModule],
-  declarations: [TaskPageComponent, TaskCardComponent],
+  declarations: [TaskPageComponent, TaskCardComponent, TaskFormComponent],
   exports: [TaskPageComponent],
   providers: [TaskService]
 })
