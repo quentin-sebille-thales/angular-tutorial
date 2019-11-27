cdb29c0bd8faa0ca828ed13a857cce9f0803c6a0
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-27 13:21:20 +0100

    4 files modified

--- src/task/components/task-form/task-form.component.ts
+++ src/task/components/task-form/task-form.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Task } from "../../models/task";
 
 @Component({
-  selector: 'app-task-form',
-  templateUrl: './task-form.component.html',
-  styleUrls: ['./task-form.component.css']
+  selector: "app-task-form",
+  templateUrl: "./task-form.component.html",
+  styleUrls: ["./task-form.component.css"]
 })
 export class TaskFormComponent implements OnInit {
+  @Input() task: Task;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
