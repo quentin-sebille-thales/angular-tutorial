ff9b091a76f7431552d5f6fc246dd5a61371cd4a
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-27 20:23:48 +0100

    1 file modified

--- src/task/services/task.service.ts
+++ src/task/services/task.service.ts
@@ -8,10 +8,15 @@ export class TaskService {
   constructor() {
     this.list = [];
     this.list.push(new Task());
+    this.list.push(new Task());
     this.list[0].id = 0;
     this.list[0].name = "Faire la roue";
     this.list[0].description = "Aller voir Michaël Ayeng pour apprendre"
     this.list[0].difficulty = 6;
+    this.list[1].id = 1;
+    this.list[1].name = "Apporter les croissants";
+    this.list[1].description = "Ca fait toujours plaisir aux collègues de temps en temps"
+    this.list[1].difficulty = 2;
   }
 
   public addTask(task: Task): void {
