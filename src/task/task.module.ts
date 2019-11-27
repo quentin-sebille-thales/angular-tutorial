c07ffb0e8617ed64cae26b79be00225797cce2de
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-27 13:32:04 +0100

    3 files modified

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
 
 import { TaskPageComponent } from "./components/task-page/task-page.component";
-import { TaskService } from './services/task.service';
-import { TaskCardComponent } from './components/task-card/task-card.component';
-import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskService } from "./services/task.service";
+import { TaskCardComponent } from "./components/task-card/task-card.component";
+import { TaskFormComponent } from "./components/task-form/task-form.component";
 
 @NgModule({
-  imports: [CommonModule, AngularMaterialModule],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   declarations: [TaskPageComponent, TaskCardComponent, TaskFormComponent],
   exports: [TaskPageComponent],
   providers: [TaskService]
