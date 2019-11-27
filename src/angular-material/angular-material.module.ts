b2aa4a1fab3b412bdc26e25ef4399ad4e3a21e59
quentin-sebille-thales <quentin.sebille@mythalesgroup.io>
2019-11-27 11:39:15 +0100

    3 files modified

--- src/angular-material/angular-material.module.ts
+++ src/angular-material/angular-material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
-import { MatCardModule, MatButtonModule } from "@angular/material";
-import { MatIconModule } from "@angular/material/icon";
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatIconModule
+} from "@angular/material";
 
 @NgModule({
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
   exports: [MatCardModule, MatButtonModule, MatIconModule]
 })
 export class AngularMaterialModule {}
